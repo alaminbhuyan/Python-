8e8d64d1bb48f1c486db2b69a59e077e62283689
al-aminbhuyan <alaminbhuyan321@gmail.com>
2020-04-02 06:11:23 +0600

    add file program-tuple-example.py

--- program/tuple/example.py
+++ program/tuple/example.py
@@ -1,16 +1,3 @@
-# from collections import Counter
-# lis= [2 ,3 ,4 ,5 ,6 ,8 ,7 ,6 ,5 ,18]
-# b=Counter(lis)
-# print(b)
-#
-# m = [[6,1],[6,1],[6,2]]
-# print(*m)
-# sum = 0
-# for i in m:
-#     for j in i:
-#         if j == b.keys():
-#             sum = sum+j[1]
-# print(sum
 from collections import Counter
 
 num_of_shoes = int(input())
